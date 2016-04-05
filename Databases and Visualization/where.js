cf24c20c581134616d13be72e826212540affa48
王兴 <wangx1ng@yahoo.com>
2016-04-05 15:12:25 +0800

    Databases and Visualization

---
+++ Databases and Visualization/where.js
@@ -0,0 +1,7 @@
+myData = [
+[42.340082,-71.0894884, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India'],
+[33.1561058,131.826132, 'Japan, 〒875-0002 Ōita-ken, Usuki-shi, Shitanoe, 1232−2 ＵＭＤ']
+];
